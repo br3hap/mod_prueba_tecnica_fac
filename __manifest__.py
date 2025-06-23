20ce194840c41730d46fab17d47b403ec791d60d
Breithner <Breithnerap@gmail.com>
2025-06-23 11:55:49 -0500

    upload change

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Prueba Técnica Fac',
+    'version': '1.0',
+    'summary': """ Módulo de una prueba técnica - odoo 18""",
+    'author': 'Breithner Aquituari',
+    'website': '',
+    'category': '',
+    'depends': ['base', ],
+    'data': [
+        
+    ],
+    
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
